6649504561133df1319bfdca7a7291bd362b3d05
godcong <jumbycc@163.com>
2020-02-11 15:07:57 +0800

    upodate accipfs

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/glvd/accipfs
 
 go 1.13
 
-require github.com/goextension/log v0.0.2 // indirect
+require (
+	github.com/goextension/log v0.0.2
+	github.com/ipfs/go-ipfs-http-client v0.0.5
+)
--- service/node_ipfs.go
+++ service/node_ipfs.go
@@ -1,5 +1,33 @@
 package service
 
+import (
+	"github.com/glvd/accipfs/config"
+	"github.com/ipfs/go-ipfs-http-client"
+	"github.com/multiformats/go-multiaddr"
+)
+
 type nodeIPFS struct {
-	path string
+	cfg config.Config
+	api *httpapi.HttpApi
+}
+
+// Start ...
+func (n nodeIPFS) Start() {
+	panic("implement me")
+}
+
+// New ...
+func NodeIPFS(config config.Config) (Node, error) {
+	ma, err := multiaddr.NewMultiaddr("path")
+	if err != nil {
+		return nil, err
+	}
+	api, err := httpapi.NewApi(ma)
+	if err != nil {
+		return nil, err
+	}
+	return &nodeIPFS{
+		cfg: config,
+		api: api,
+	}, nil
 }

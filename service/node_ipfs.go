b03dd8a9c9f002b28891be956d956353609d304d
godcong <jumbycc@163.com>
2020-02-11 15:15:21 +0800

    upodate accipfs

--- service/node_ipfs.go
+++ service/node_ipfs.go
@@ -3,7 +3,6 @@ package service
 import (
 	"github.com/glvd/accipfs/config"
 	"github.com/ipfs/go-ipfs-http-client"
-	"github.com/multiformats/go-multiaddr"
 )
 
 type nodeIPFS struct {
@@ -18,16 +17,7 @@ func (n nodeIPFS) Start() {
 
 // NodeIPFS ...
 func NodeIPFS(config config.Config) (Node, error) {
-	ma, err := multiaddr.NewMultiaddr("path")
-	if err != nil {
-		return nil, err
-	}
-	api, err := httpapi.NewApi(ma)
-	if err != nil {
-		return nil, err
-	}
 	return &nodeIPFS{
 		cfg: config,
-		api: api,
 	}, nil
 }

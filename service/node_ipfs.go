e0063502188334e9b4a4401088eca3915b002d8d
godcong <jumbycc@163.com>
2020-02-12 21:57:51 +0800

    update accipfs

--- service/node_ipfs.go
+++ service/node_ipfs.go
@@ -1,8 +1,11 @@
 package service
 
 import (
+	"context"
 	"github.com/glvd/accipfs/config"
 	"github.com/ipfs/go-ipfs-http-client"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/multiformats/go-multiaddr"
 )
 
 type nodeIPFS struct {
@@ -14,4 +17,20 @@ func newNodeIPFS(config config.Config) (*nodeIPFS, error) {
 	return &nodeIPFS{
 		cfg: config,
 	}, nil
+}
+
+func (i *nodeIPFS) SwarmConnect(ctx context.Context, addr string) (e error) {
+	ma, e := multiaddr.NewMultiaddr(addr)
+	if e != nil {
+		return e
+	}
+	info, e := peer.AddrInfoFromP2pAddr(ma)
+	if e != nil {
+		return e
+	}
+	e = i.api.Swarm().Connect(ctx, *info)
+	if e != nil {
+		return e
+	}
+	return nil
 }

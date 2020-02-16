4b44340b2c42986f4aad537030d7a1bdc0ce3e28
godcong <jumbycc@163.com>
2020-02-16 21:42:09 +0800

    update accipfs

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/glvd/accipfs
 go 1.13
 
 require (
+	github.com/ethereum/go-ethereum v1.9.10
 	github.com/goextension/log v0.0.2
 	github.com/ipfs/go-ipfs-http-client v0.0.5
+	github.com/ipfs/interface-go-ipfs-core v0.2.3
+	github.com/libp2p/go-libp2p-core v0.2.3
+	github.com/multiformats/go-multiaddr v0.1.1
 )
--- service/node_eth.go
+++ service/node_eth.go
@@ -1,16 +1,26 @@
 package service
 
 import (
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/glvd/accipfs/config"
+	"path/filepath"
 )
 
 const ethPath = ".ethereum"
 const endPoint = "geth.ipc"
 
 type nodeETH struct {
-	cfg config.Config
+	cfg    config.Config
+	client *ethclient.Client
 }
 
-func newETH(cfg config.Config) *nodeETH {
-	return &nodeETH{cfg: cfg}
+func newETH(cfg config.Config) (*nodeETH, error) {
+	client, e := ethclient.Dial(filepath.Join(cfg.Path, ethPath, endPoint))
+	if e != nil {
+		return nil, e
+	}
+	return &nodeETH{
+		cfg:    cfg,
+		client: client,
+	}, nil
 }

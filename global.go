5adbccb880b55bcafa8c12755c2f0e3692624dff
godcong <jumbycc@163.com>
2020-02-17 14:17:48 +0800

    upodate accipfs

--- global.go
+++ global.go
@@ -1,4 +1,24 @@
 package accipfs
 
+import (
+	"fmt"
+	"os/exec"
+)
+
 // DefaultPath ...
 var DefaultPath = "."
+
+// CheckPort ...
+func CheckPort(port int) error {
+	checkStatement := fmt.Sprintf("netstat -anp | grep -q %d ", port)
+	output, err := exec.Command("sh", "-c", checkStatement).CombinedOutput()
+	if err != nil {
+		return nil
+	}
+
+	if len(output) > 0 {
+		return fmt.Errorf("port %d already occupied", port)
+	}
+
+	return nil
+}

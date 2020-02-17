a515535a7a25a7132f7f26f8b2513cea8b0abe6b
godcong <jumbycc@163.com>
2020-02-17 14:50:24 +0800

    upodate accipfs

--- console/main.go
+++ console/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
 
+var rootCmd = &cobra.Command{
+	Use:   "accipfs",
+	Short: "accipfs is a very fast ipfs client",
+	Long: `A Fast and Flexible Static Site Generator built with
+                love by spf13 and friends in Go.
+                Complete documentation is available at http://hugo.spf13.com`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+// Execute ...
+func Execute() {
+
+}
 func main() {
 	fmt.Println("accipfs starting...")
+
+	if err := rootCmd.Execute(); err != nil {
+		panic(err)
+	}
+
 }

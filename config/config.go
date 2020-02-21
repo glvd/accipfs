64ff7cc8b01e159bf1682d94dfd585d64671bfd3
godcong <jumbycc@163.com>
2020-02-21 21:44:11 +0800

    update accipfs

--- config/config.go
+++ config/config.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"fmt"
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
 // IPFSConfig ...
 type IPFSConfig struct {
 	Name string
@@ -21,7 +27,28 @@ type Config struct {
 }
 
 // LoadConfig ...
-func LoadConfig() *Config {
+func LoadConfig(path string) (*Config, error) {
+	if path != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(path)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			return nil, err
+		}
+
+		// Search config in home directory with name ".cobra" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".cobra")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+
 	return &Config{
 		Path: "",
 		ETH: ETHConfig{
@@ -33,5 +60,5 @@ func LoadConfig() *Config {
 		},
 		AwsAccessKeyID:     "",
 		AwsSecretAccessKey: "",
-	}
+	}, nil
 }
--- go.mod
+++ go.mod
@@ -8,5 +8,8 @@ require (
 	github.com/ipfs/go-ipfs-http-client v0.0.5
 	github.com/ipfs/interface-go-ipfs-core v0.2.3
 	github.com/libp2p/go-libp2p-core v0.2.3
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/multiformats/go-multiaddr v0.1.1
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.3.2
 )

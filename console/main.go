da2bda6f4585749d902d65d62edba6344acf1987
godcong <jumbycc@163.com>
2020-03-02 14:42:03 +0800

    upodate accipfs

--- console/main.go
+++ console/main.go
@@ -5,8 +5,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// APP ...
+const APP = "accipfs"
+
+// Version ...
+const Version = "0.0.1"
+
 var rootCmd = &cobra.Command{
-	Use:   "accipfs",
+	Use:   APP,
 	Short: "accipfs is a very fast ipfs client",
 	Long:  `accipfs`,
 	Run: func(cmd *cobra.Command, args []string) {
@@ -16,9 +22,21 @@ var rootCmd = &cobra.Command{
 
 func main() {
 	fmt.Println("accipfs starting...")
-
+	rootCmd.AddCommand(daemonRun())
 	if err := rootCmd.Execute(); err != nil {
 		panic(err)
 	}
 
+}
+
+func versionCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number of " + APP,
+		Long:  `All software has versions. This is ` + APP + `'s`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println(APP, "Version:", Version)
+		},
+	}
+	return cmd
 }

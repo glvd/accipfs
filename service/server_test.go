cfee43dddd1027fefd7e86be2f876c8e0d190304
godcong <jumbycc@163.com>
2020-03-12 17:33:21 +0800

    upodate accipfs

--- service/server_test.go
+++ service/server_test.go
@@ -4,7 +4,6 @@ import (
 	"github.com/glvd/accipfs/config"
 	"github.com/goextension/log/zap"
 	"testing"
-	"time"
 )
 
 func init() {
@@ -20,36 +19,36 @@ func TestNodeServerETH(t *testing.T) {
 	config.Initialize()
 	eth := NewNodeServerETH(config.Global())
 	t.Logf("%+v", eth)
-	//if err := eth.Init(); err != nil {
-	//	t.Error(err)
-	//	return
-	//}
-	if err := eth.Start(); err != nil {
+	if err := eth.Init(); err != nil {
 		t.Error(err)
 		return
 	}
+	//if err := eth.Start(); err != nil {
+	//	t.Error(err)
+	//	return
+	//}
 
 	ipfs := NewNodeServerIPFS(config.Global())
 	t.Logf("%+v", ipfs)
-	//if err := ipfs.Init(); err != nil {
+	if err := ipfs.Init(); err != nil {
+		t.Error(err)
+		return
+	}
+	//if err := ipfs.Start(); err != nil {
 	//	t.Error(err)
 	//	return
 	//}
-	if err := ipfs.Start(); err != nil {
-		t.Error(err)
-		return
-	}
 
-	time.Sleep(1 * time.Minute)
+	//time.Sleep(1 * time.Minute)
 
-	if err := eth.Stop(); err != nil {
-		t.Error(err)
-		return
-	}
-
-	if err := ipfs.Stop(); err != nil {
-		t.Error(err)
-		return
-	}
+	//if err := eth.Stop(); err != nil {
+	//	t.Error(err)
+	//	return
+	//}
+	//
+	//if err := ipfs.Stop(); err != nil {
+	//	t.Error(err)
+	//	return
+	//}
 	t.Log("done")
 }

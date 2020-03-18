8973a28b8feeb85ead9d9db6b386bf8eda53419c
godcong <jumbycc@163.com>
2020-03-18 14:33:04 +0800

    upodate accipfs

--- core/node_store.go
+++ core/node_store.go
@@ -70,24 +70,24 @@ type NodeInfo struct {
 	Version   string
 }
 
-// AccelerateCache ...
-type AccelerateCache struct {
+// NodeStore ...
+type NodeStore struct {
 	nodes sync.Map
 }
 
 // Add ...
-func (c *AccelerateCache) Add(info *NodeInfo) {
+func (c *NodeStore) Add(info *NodeInfo) {
 	c.nodes.Store(info.Name, info)
 }
 
 // Check ...
-func (c *AccelerateCache) Check(key string) (b bool) {
+func (c *NodeStore) Check(key string) (b bool) {
 	_, b = c.nodes.Load(key)
 	return
 }
 
 // Get ...
-func (c *AccelerateCache) Get(key string) *NodeInfo {
+func (c *NodeStore) Get(key string) *NodeInfo {
 	if v, b := c.nodes.Load(key); b {
 		return v.(*NodeInfo)
 	}
@@ -95,7 +95,7 @@ func (c *AccelerateCache) Get(key string) *NodeInfo {
 }
 
 // Range ...
-func (c *AccelerateCache) Range(f func(info *NodeInfo) bool) {
+func (c *NodeStore) Range(f func(info *NodeInfo) bool) {
 	c.nodes.Range(func(key, value interface{}) bool {
 		return f(value.(*NodeInfo))
 	})

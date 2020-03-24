ff7c7d5b6bfa253d15c1a4f20a7a2fd0830a23ba
godcong <jumbycc@163.com>
2020-03-24 12:58:59 +0800

    upodate accipfs

--- console/tag.go
+++ console/tag.go
@@ -3,7 +3,14 @@ package main
 import "github.com/spf13/cobra"
 
 func tagCmd() *cobra.Command {
-	return &cobra.Command{}
+	return &cobra.Command{
+		Use:   "tag",
+		Short: "tag contract",
+		Long:  "tag contract manages all you videos",
+		Run: func(cmd *cobra.Command, args []string) {
+
+		},
+	}
 }
 
 func tagListCmd() *cobra.Command {

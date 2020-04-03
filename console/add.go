26abf5e28b97acb1e4731c5b3cd76cfb725f4908
godcong <jumbycc@163.com>
2020-04-04 04:30:53 +0800

    update accipfs

--- console/add.go
+++ console/add.go
@@ -5,12 +5,18 @@ import (
 )
 
 func addCmd() *cobra.Command {
-	return &cobra.Command{
+	var path string
+	var info string
+	cmd := &cobra.Command{
 		Use:   "add",
 		Short: "add a source to this node",
 		Long:  "add a source to this node for service some users by your self",
 		Run: func(cmd *cobra.Command, args []string) {
 			//add a file with rule to accipfs
+
 		},
 	}
+	cmd.Flags().StringVar(&path, "path", "", "set the file dirctory path to add")
+	cmd.Flags().StringVar(&info, "info", "", "set the file info to load")
+	return cmd
 }

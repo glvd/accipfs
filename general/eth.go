c2bd5ab18ed58a98ff66fd5fa71f70f2ca52e5c2
godcong <jumbycc@163.com>
2020-04-05 22:07:25 +0800

    update accipfs

--- general/eth.go
+++ general/eth.go
@@ -1,5 +1,29 @@
 package general
 
+// Alloc ...
+type Alloc struct {
+	Balance string `json:"balance"`
+}
+
+// Genesis ...
+type Genesis struct {
+	Config struct {
+		ChainID             int `json:"chainId"`
+		HomesteadBlock      int `json:"homesteadBlock"`
+		Eip150Block         int `json:"eip150Block"`
+		Eip155Block         int `json:"eip155Block"`
+		Eip158Block         int `json:"eip158Block"`
+		ByzantiumBlock      int `json:"byzantiumBlock"`
+		ConstantinopleBlock int `json:"constantinopleBlock"`
+		PetersburgBlock     int `json:"petersburgBlock"`
+		Ethash              struct {
+		} `json:"ethash"`
+	} `json:"config"`
+	Difficulty string            `json:"difficulty"`
+	GasLimit   string            `json:"gasLimit"`
+	Alloc      map[string]*Alloc `json:"alloc"`
+}
+
 // LoadGenesis ...
 func LoadGenesis() {
 

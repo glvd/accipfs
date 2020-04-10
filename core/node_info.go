149a71eafed4cff587ce6eedfa04b6ef2da53dab
godcong <jumbycc@163.com>
2020-04-10 16:35:16 +0800

    upodate accipfs

--- core/node_info.go
+++ core/node_info.go
@@ -1,10 +1,21 @@
 package core
 
+// NodeType ...
+type NodeType int
+
+const (
+	// NodeUndefined ...
+	NodeUndefined NodeType = -1
+	// NodeAccount ...
+	NodeAccount NodeType = iota
+)
+
 // NodeInfo ...
 type NodeInfo struct {
 	Name       string
 	Schema     string
 	RemoteAddr string
+	NodeType   string
 	Port       int
 	Contract   ContractNode
 	DataStore  DataStoreNode

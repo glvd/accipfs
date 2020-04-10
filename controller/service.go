6831af3bacffda4f3a91e954a372bdb92445a3f6
godcong <jumbycc@163.com>
2020-04-10 13:48:24 +0800

    upodate accipfs

--- controller/service.go
+++ controller/service.go
@@ -1,6 +1,9 @@
 package controller
 
-import "github.com/glvd/accipfs/config"
+import (
+	"github.com/glvd/accipfs/config"
+	"sync"
+)
 
 // ServiceIndex ...
 type ServiceIndex int
@@ -21,6 +24,7 @@ type Service interface {
 
 // Controller ...
 type Controller struct {
+	wg       *sync.WaitGroup
 	services map[ServiceIndex]Service
 }
 
@@ -29,12 +33,32 @@ func New(cfg *config.Config) *Controller {
 	c := &Controller{
 		services: map[ServiceIndex]Service{},
 	}
+	c.wg = &sync.WaitGroup{}
 	c.services[IndexETH] = newNodeBinETH(cfg)
 	c.services[IndexIPFS] = newNodeBinIPFS(cfg)
 	return c
 }
 
+// Init ...
+func (c *Controller) Init() (e error) {
+	for _, service := range c.services {
+		e = service.Init()
+		if e != nil {
+			return e
+		}
+	}
+	return
+}
+
 // Run ...
 func (c *Controller) Run() {
-
+	for _, service := range c.services {
+		c.wg.Add(1)
+		go func() {
+			defer c.wg.Done()
+			if err := service.Start(); err != nil {
+				return
+			}
+		}()
+	}
 }

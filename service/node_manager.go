39ca159df0546f0b2e5ab0a99397a346edced3d4
godcong <jumbycc@163.com>
2020-04-12 03:05:10 +0800

    update accipfs

--- service/node_manager.go
+++ service/node_manager.go
@@ -5,3 +5,13 @@ import "github.com/glvd/accipfs/core"
 type nodeManager struct {
 	nodes map[string]core.NodeInfo
 }
+
+// Add ...
+func (m *nodeManager) Add(info *core.NodeInfo) {
+
+}
+
+// Get ...
+func (m *nodeManager) Get(name string) *core.NodeInfo {
+	return nil
+}

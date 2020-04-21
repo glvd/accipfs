e8446f86e665cf33f61efbd4e077fc63f3718aee
godcong <jumbycc@163.com>
2020-04-21 14:03:48 +0800

    upodate accipfs

--- console/mdns/main.go
+++ console/mdns/main.go
@@ -3,12 +3,23 @@ package main
 import (
 	"github.com/glvd/accipfs/config"
 	"github.com/glvd/accipfs/mdns"
+	"net"
 	"time"
 )
 
 func main() {
 	m, err := mdns.New(config.Default(), func(cfg *mdns.OptionConfig) {
 		cfg.Service = "_foobar._tcp"
+		addrs, err := net.InterfaceAddrs()
+		if err != nil {
+			return
+		}
+		for i := range addrs {
+			cidr, _, err := net.ParseCIDR(addrs[i].String())
+			if err == nil {
+				cfg.IPs = append(cfg.IPs, cidr)
+			}
+		}
 	})
 	if err != nil {
 		panic(err)

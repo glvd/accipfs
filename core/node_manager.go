24dbe325909cb75188bf8f384c5d735ecfc1cdef
godcong <jumbycc@163.com>
2020-06-12 15:56:00 +0800

    update accipfs

--- core/node_manager.go
+++ core/node_manager.go
@@ -3,7 +3,7 @@ package core
 // NodeManager ...
 type NodeManager interface {
 	Push(node Node)
-	Range(f func(node Node))
+	Range(f func(key string, node Node) bool)
 	Store() error
 	Load() error
 }

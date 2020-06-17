a2833cb42ed921bdb294dddfbe757f2e48133a79
godcong <jumbycc@163.com>
2020-06-17 13:57:02 +0800

    update accipfs

--- global_test.go
+++ global_test.go
@@ -2,19 +2,11 @@ package accipfs_test
 
 import (
 	"github.com/glvd/accipfs/config"
-	"github.com/glvd/accipfs/service"
 	"testing"
 )
 
 func TestRun(t *testing.T) {
 	config.WorkDir = "D:\\workspace\\pvt"
 	config.Initialize()
-	cfg := config.Global()
-	s, e := service.New(&cfg)
-	t.Log("accelerate new")
-	if e != nil {
-		panic(e)
-	}
-	defer s.Stop()
-	s.Start()
+
 }

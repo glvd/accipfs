f5835de95dca939d4760ae9f73583faa43efcda9
godcong <jumbycc@163.com>
2020-06-19 13:54:16 +0800

    update accipfs

--- node/node.go
+++ node/node.go
@@ -1,20 +1,32 @@
 package node
 
 import (
+	"bytes"
 	"github.com/glvd/accipfs/core"
 	"github.com/glvd/accipfs/general"
 	"github.com/portmapping/go-reuse"
+	"go.uber.org/atomic"
 	"net"
+	"sync"
 )
+
+const maxByteSize = 65520
 
 type jsonNode struct {
 	Addrs []core.Addr `json:"addrs"`
 }
 
 type node struct {
-	id    string
-	addrs []core.Addr
-	conn  net.Conn
+	id        string
+	addrs     []core.Addr
+	isRunning *atomic.Bool
+	isAccept  bool
+	conn      net.Conn
+}
+
+// IsConnecting ...
+func (n *node) IsConnecting() bool {
+	panic("implement me")
 }
 
 var _ core.Node = &node{}
@@ -49,12 +61,31 @@ func ConnectToNode(addr core.Addr, bind int) (core.Node, error) {
 	}, nil
 }
 
-func (n *node) recv() {
+func (n *node) recv(wg *sync.WaitGroup, b chan<- []byte) {
+	defer wg.Done()
+	for {
+		tmp := make([]byte, maxByteSize)
+		read, err := n.conn.Read(tmp)
+		if err != nil {
+			return
+		}
+		log.Debugw("recv", "read", read)
+		b <- tmp
 
+	}
 }
 
-func (n *node) send() {
-
+func (n *node) send(wg *sync.WaitGroup, b <-chan []byte) {
+	defer wg.Done()
+	tmp := make([]byte, maxByteSize)
+	for {
+		copy(tmp, <-b)
+		write, err := n.conn.Write(tmp)
+		if err != nil {
+			return
+		}
+		log.Debugw("send", "write", write)
+	}
 }
 
 func nodeRun(node *node) (core.Node, error) {
@@ -67,7 +98,9 @@ func AcceptNode(conn net.Conn) (core.Node, error) {
 	addr := conn.RemoteAddr()
 	ip, port := general.SplitIP(addr.String())
 	return nodeRun(&node{
-		id: "", //todo
+		id:        "", //todo
+		isRunning: atomic.NewBool(false),
+		isAccept:  true,
 		addrs: []core.Addr{
 			{
 				Protocol: addr.Network(),
@@ -100,5 +133,31 @@ func (n *node) Ping() error {
 }
 
 func (n *node) running() {
+	if n.isRunning.Load() {
+		return
+	}
+	n.isRunning.Store(true)
+	defer func() {
+		if n.conn != nil {
+			n.conn.Close()
+		}
+	}()
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	cache := make(map[int]bytes.Buffer)
 
+	recvData := make(chan []byte)
+	sendData := make(chan []byte)
+	go n.recv(wg, recvData)
+	go n.send(wg, sendData)
+	for {
+
+		select {
+		case snd <- buf:
+		case buf1 := <-rec:
+			fmt.Println("rec", string(buf1))
+		}
+	}
+
+	wg.Wait()
 }

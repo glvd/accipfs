3497429a1f72b5375b98694d6ab53a5c5cf29eeb
godcong <jumbycc@163.com>
2020-06-20 13:31:55 +0800

    update accipfs

--- node/version_test.go
+++ node/version_test.go
@@ -1,6 +1,7 @@
 package node
 
 import (
+	"bytes"
 	"strings"
 	"testing"
 )
@@ -10,4 +11,27 @@ func TestVersion_String(t *testing.T) {
 		'v', 1, 3, 5,
 	}
 	t.Log(strings.Compare(v.String(), "v1.3.5"))
+}
+
+func TestParseVersion(t *testing.T) {
+	version, err := ParseVersion("v1.3.5")
+	if err != nil {
+		t.Fatal(err)
+	}
+	b := [4]byte{
+		'v', 1, 3, 5,
+	}
+
+	t.Log(bytes.Compare(version[:], b[:]))
+
+	version1, err := ParseVersion("v1.3")
+	if err != nil {
+		t.Fatal(err)
+	}
+	b1 := [4]byte{
+		'v', 1, 3,
+	}
+
+	t.Log(bytes.Compare(version1[:], b1[:]))
+
 }

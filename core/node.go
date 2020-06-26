03482604ab3b2443af227284f4dbcffee889bb2d
godcong <jumbycc@163.com>
2020-06-26 23:15:48 +0800

    update accipfs

--- core/node.go
+++ core/node.go
@@ -1,8 +1,11 @@
 package core
 
+type RecvCBFunc func(id string, v interface{}) [()]byte, error)
+
 // Node ...
 type Node interface {
 	ID() string
+	RecvCallback(fn RecvCBFunc)
 	Addrs() []Addr
 	Info() NodeInfo
 	Ping() error

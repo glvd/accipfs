35c08617b2a6aea3b133e5196d37b667f9eddbca
godcong <jumbycc@163.com>
2020-06-29 17:40:34 +0800

    update accipfs

--- node/manager_test.go
+++ node/manager_test.go
@@ -1,9 +1,12 @@
 package node
 
 import (
+	"fmt"
 	"github.com/glvd/accipfs/config"
 	"github.com/glvd/accipfs/controller"
+	"github.com/glvd/accipfs/core"
 	alog "github.com/glvd/accipfs/log"
+	ma "github.com/multiformats/go-multiaddr"
 	"testing"
 	"time"
 )
@@ -16,11 +19,36 @@ func init() {
 	testConfig.Node.BackupSeconds = 3 * time.Second
 }
 
+func TestManager_Store(t *testing.T) {
+	cfg := config.Default()
+	nodeManager := New(cfg, &controller.Controller{})
+	multiaddr, err := ma.NewMultiaddr("/ip4/127.0.0.1/tcp/12345")
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < 100; i++ {
+		connectNode, err := ConnectNode(multiaddr, 0, &dummyAPI{})
+		if err != nil {
+			continue
+		}
+		nodeManager.Push(connectNode)
+		err = nodeManager.Store()
+		if err != nil {
+			continue
+		}
+	}
+}
 func TestManager_Load(t *testing.T) {
 	cfg := config.Default()
 	nodeManager := New(cfg, &controller.Controller{})
-	err := nodeManager.Load()
-	if err != nil {
-		panic(err)
+	for i := 0; i < 100; i++ {
+		err := nodeManager.Load()
+		if err != nil {
+			continue
+		}
+		nodeManager.Range(func(key string, n core.Node) bool {
+			fmt.Println("key:", key, "node", n.ID())
+			return true
+		})
 	}
 }

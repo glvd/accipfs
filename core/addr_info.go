8b3ef5d6d877605f04af74e76ce6f29f2e17336f
godcong <jumbycc@163.com>
2020-06-30 17:43:39 +0800

    update accipfs

--- core/addr_info.go
+++ core/addr_info.go
@@ -4,11 +4,41 @@ import ma "github.com/multiformats/go-multiaddr"
 
 // AddrInfo ...
 type AddrInfo struct {
-	ID    string
-	Addrs map[ma.Multiaddr]bool
+	id    string
+	addrs map[ma.Multiaddr]bool
+}
+
+// NewAddrInfo ...
+func NewAddrInfo(id string, addrs ...ma.Multiaddr) *AddrInfo {
+	_addrs := make(map[ma.Multiaddr]bool, len(addrs))
+	for _, addr := range addrs {
+		_addrs[addr] = true
+	}
+	return &AddrInfo{
+		id:    id,
+		addrs: _addrs,
+	}
+}
+
+// SetID ...
+func (info *AddrInfo) SetID(id string) {
+	info.id = id
+}
+
+// ID ...
+func (info *AddrInfo) ID() string {
+	return info.id
 }
 
 // Append ...
 func (info *AddrInfo) Append(multiaddr ma.Multiaddr) {
-	info.Addrs[multiaddr] = true
+	info.addrs[multiaddr] = true
+}
+
+// Addrs ...
+func (info *AddrInfo) Addrs() (addrs []ma.Multiaddr) {
+	for addr := range info.addrs {
+		addrs = append(addrs, addr)
+	}
+	return
 }

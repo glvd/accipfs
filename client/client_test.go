fce92203d58e8b355402cb5a10c78ee64489ac12
godcong <jumbycc@163.com>
2020-07-25 19:32:13 +0800

    update accipfs

--- client/client_test.go
+++ client/client_test.go
@@ -1,19 +1,33 @@
 package client
 
 import (
-	"github.com/glvd/accipfs/config"
-	"github.com/glvd/accipfs/core"
+	"context"
+	httpapi "github.com/ipfs/go-ipfs-http-client"
+	ma "github.com/multiformats/go-multiaddr"
 	"testing"
 )
 
-func init() {
-	DefaultClient = New(config.Default())
+var _api = defaultAPI()
+
+func defaultAPI() *httpapi.HttpApi {
+	multiaddr, err := ma.NewMultiaddr("/ip4/127.0.0.1/tcp/5001")
+	if err != nil {
+		panic(err)
+	}
+	api, err := httpapi.NewApi(multiaddr)
+	if err != nil {
+		panic(err)
+	}
+	return api
 }
 
-func TestDataStorePinLs(t *testing.T) {
-	ls, err := DefaultClient.DataStoreAPI().PinLs(&core.DataStoreReq{})
+func TestIPFSPinLs(t *testing.T) {
+	ls, err := _api.Pin().Ls(context.TODO())
 	if err != nil {
 		t.Fatal(err)
 	}
-	t.Log(ls)
+	for ps := range ls {
+		t.Logf("%+v",ps.Path().String())
+	}
+
 }

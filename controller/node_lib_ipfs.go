0ddd907d93dc6cbad7be53cd41cf292afd59ea18
godcong <jumbycc@163.com>
2020-07-28 11:46:43 +0800

    update accipfs

--- controller/node_lib_ipfs.go
+++ controller/node_lib_ipfs.go
@@ -1,10 +1,19 @@
 package controller
 
-import "github.com/glvd/accipfs/config"
+import (
+	"context"
+	"github.com/glvd/accipfs/config"
+)
 
 type nodeLibIPFS struct {
+	ctx    context.Context
+	cancel context.CancelFunc
 }
 
 func newNodeLibIPFS(cfg *config.Config) *nodeLibIPFS {
-	return &nodeLibIPFS{}
+	ctx, cancel := context.WithCancel(context.Background())
+	return &nodeLibIPFS{
+		ctx:    ctx,
+		cancel: cancel,
+	}
 }

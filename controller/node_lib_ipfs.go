b002270933db6bff34114b6fb3c9dee79a2222d9
godcong <jumbycc@163.com>
2020-07-28 11:54:00 +0800

    update accipfs

--- controller/node_lib_ipfs.go
+++ controller/node_lib_ipfs.go
@@ -2,7 +2,19 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"github.com/ipfs/go-ipfs/core/coreapi"
+	"github.com/ipfs/go-ipfs/core/node/libp2p"
+	"github.com/ipfs/go-ipfs/repo/fsrepo"
+	"io/ioutil"
+	"path/filepath"
+
 	"github.com/glvd/accipfs/config"
+	"github.com/glvd/accipfs/core"
+	ipfsconfig "github.com/ipfs/go-ipfs-config"
+	ipfscore "github.com/ipfs/go-ipfs/core"
+	"github.com/ipfs/go-ipfs/plugin/loader"
+	interfacecore "github.com/ipfs/interface-go-ipfs-core"
 )
 
 type nodeLibIPFS struct {
@@ -10,10 +22,120 @@ type nodeLibIPFS struct {
 	cancel context.CancelFunc
 }
 
+var _ core.ControllerService = &nodeLibIPFS{}
+
 func newNodeLibIPFS(cfg *config.Config) *nodeLibIPFS {
 	ctx, cancel := context.WithCancel(context.Background())
+
 	return &nodeLibIPFS{
 		ctx:    ctx,
 		cancel: cancel,
 	}
+}
+
+// Start ...
+func (n nodeLibIPFS) Start() error {
+	panic("implement me")
+}
+
+// Stop ...
+func (n nodeLibIPFS) Stop() error {
+	panic("implement me")
+}
+
+// Initialize ...
+func (n nodeLibIPFS) Initialize() error {
+	panic("implement me")
+}
+
+// IsReady ...
+func (n nodeLibIPFS) IsReady() bool {
+	panic("implement me")
+}
+
+// MessageHandle ...
+func (n nodeLibIPFS) MessageHandle(f func(s string)) {
+	panic("implement me")
+}
+
+func setupPlugins(externalPluginsPath string) error {
+	// Load any external plugins if available on externalPluginsPath
+	plugins, err := loader.NewPluginLoader(filepath.Join(externalPluginsPath, "plugins"))
+	if err != nil {
+		return fmt.Errorf("error loading plugins: %s", err)
+	}
+
+	// Load preloaded and external plugins
+	if err := plugins.Initialize(); err != nil {
+		return fmt.Errorf("error initializing plugins: %s", err)
+	}
+
+	if err := plugins.Inject(); err != nil {
+		return fmt.Errorf("error initializing plugins: %s", err)
+	}
+
+	return nil
+}
+
+// Spawns a node to be used just for this run (i.e. creates a tmp repo)
+func spawnEphemeral(ctx context.Context) (interfacecore.CoreAPI, error) {
+	if err := setupPlugins(""); err != nil {
+		return nil, err
+	}
+
+	// Create a Temporary Repo
+	repoPath, err := createTempRepo(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create temp repo: %s", err)
+	}
+
+	// Spawning an ephemeral IPFS node
+	return createNode(ctx, repoPath)
+}
+
+func createTempRepo(ctx context.Context) (string, error) {
+	repoPath, err := ioutil.TempDir("", "ipfs-shell")
+	if err != nil {
+		return "", fmt.Errorf("failed to get temp dir: %s", err)
+	}
+
+	// Create a config with default options and a 2048 bit key
+	cfg, err := ipfsconfig.Init(ioutil.Discard, 2048)
+	if err != nil {
+		return "", err
+	}
+
+	// Create the repo with the config
+	err = fsrepo.Init(repoPath, cfg)
+	if err != nil {
+		return "", fmt.Errorf("failed to init ephemeral node: %s", err)
+	}
+
+	return repoPath, nil
+}
+
+// Creates an IPFS node and returns its coreAPI
+func createNode(ctx context.Context, repoPath string) (interfacecore.CoreAPI, error) {
+	// Open the repo
+	repo, err := fsrepo.Open(repoPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// Construct the node
+
+	nodeOptions := &ipfscore.BuildCfg{
+		Online:  true,
+		Routing: libp2p.DHTOption, // This option sets the node to be a full DHT node (both fetching and storing DHT Records)
+		// Routing: libp2p.DHTClientOption, // This option sets the node to be a client DHT node (only fetching records)
+		Repo: repo,
+	}
+
+	node, err := ipfscore.NewNode(ctx, nodeOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	// Attach the Core API to the constructed node
+	return coreapi.NewCoreAPI(node)
 }
--- go.mod
+++ go.mod
@@ -17,26 +17,23 @@ require (
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/google/uuid v1.1.1
 	github.com/gorilla/rpc v1.2.0
-	github.com/ipfs/go-blockservice v0.1.3 // indirect
 	github.com/ipfs/go-cid v0.0.6
+	github.com/ipfs/go-ipfs v0.6.0
 	github.com/ipfs/go-ipfs-blockstore v1.0.0 // indirect
-	github.com/ipfs/go-ipfs-cmds v0.2.9 // indirect
 	github.com/ipfs/go-ipfs-config v0.8.0
 	github.com/ipfs/go-ipfs-files v0.0.8
 	github.com/ipfs/go-ipfs-http-client v0.0.5
-	github.com/ipfs/go-ipfs-util v0.0.2 // indirect
 	github.com/ipfs/go-ipld-cbor v0.0.4
 	github.com/ipfs/go-ipld-format v0.2.0
 	github.com/ipfs/go-log/v2 v2.1.1 // indirect
-	github.com/ipfs/go-merkledag v0.3.2 // indirect
 	github.com/ipfs/interface-go-ipfs-core v0.3.0
 	github.com/libp2p/go-libp2p-core v0.6.0
 	github.com/libp2p/go-openssl v0.0.6 // indirect
 	github.com/mattn/go-colorable v0.1.7 // indirect
-	github.com/miekg/dns v1.1.12
+	github.com/miekg/dns v1.1.29
 	github.com/mr-tron/base58 v1.2.0 // indirect
 	github.com/multiformats/go-multiaddr v0.2.2
-	github.com/multiformats/go-multiaddr-dns v0.1.0
+	github.com/multiformats/go-multiaddr-dns v0.2.0
 	github.com/multiformats/go-multiaddr-net v0.1.5
 	github.com/multiformats/go-multihash v0.0.14 // indirect
 	github.com/opentracing/opentracing-go v1.2.0 // indirect
@@ -50,7 +47,6 @@ require (
 	go.uber.org/atomic v1.6.0
 	go.uber.org/zap v1.15.0
 	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899 // indirect
-	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
 	golang.org/x/net v0.0.0-20200707034311-ab3426394381
 	golang.org/x/sys v0.0.0-20200722175500-76b94024e4b6 // indirect
 	golang.org/x/tools v0.0.0-20200702044944-0cc1aa72b347 // indirect

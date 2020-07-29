00329e03933ea226614d3b1ef8a1ef650cb733b7
godcong <jumbycc@163.com>
2020-07-29 16:26:18 +0800

    update accipfs

--- console/pin.go
+++ console/pin.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/glvd/accipfs/client"
 	"github.com/glvd/accipfs/config"
 	"github.com/glvd/accipfs/core"
 	"github.com/spf13/cobra"
+	"os"
+	"os/signal"
 )
 
 func pinCmd() *cobra.Command {
@@ -27,14 +30,31 @@ func pinLsCmd() *cobra.Command {
 			config.Initialize()
 			cfg := config.Global()
 			client.InitGlobalClient(&cfg)
-
-			pins, err := client.DataStorePinLs(&core.DataStoreReq{})
-			if err != nil {
-				panic(err)
-			}
-			fmt.Println("show pin list:")
-			for _, v := range pins.Pins {
-				fmt.Println(v)
+			ctx, cancelFunc := context.WithCancel(context.TODO())
+			done := make(chan error)
+			go func(c context.Context) {
+				var err error
+				defer func() {
+					done <- err
+				}()
+				pins, err := client.DataStorePinLs(c, &core.DataStoreReq{})
+				if err != nil {
+					panic(err)
+				}
+				fmt.Println("show pin list:")
+				for _, v := range pins.Pins {
+					fmt.Println(v)
+				}
+			}(ctx)
+			sigs := make(chan os.Signal)
+			signal.Notify(sigs, os.Interrupt)
+			select {
+			case <-sigs:
+				cancelFunc()
+			case v := <-done:
+				if v != nil {
+					panic(v)
+				}
 			}
 		},
 	}

5e3fd178eb9058897b62acb392730b7bc04e2e8c
godcong <jumbycc@163.com>
2020-07-30 15:52:52 +0800

    update accipfs

--- controller/node_lib_ipfs.go
+++ controller/node_lib_ipfs.go
@@ -3,24 +3,20 @@ package controller
 import (
 	"context"
 	"fmt"
-	"github.com/glvd/accipfs/plugin/loader"
-	ipfscore "github.com/ipfs/go-ipfs/core"
-	"github.com/ipfs/go-ipfs/repo"
-	"go.uber.org/atomic"
-	"runtime"
-	"sort"
-	"sync"
-
-	"os"
-	"path/filepath"
-
 	"github.com/glvd/accipfs/config"
 	"github.com/glvd/accipfs/core"
+	"github.com/glvd/accipfs/plugin/loader"
 	ipfsversion "github.com/ipfs/go-ipfs"
 	ipfsconfig "github.com/ipfs/go-ipfs-config"
+	ipfscore "github.com/ipfs/go-ipfs/core"
+	"github.com/ipfs/go-ipfs/repo"
 	"github.com/ipfs/go-ipfs/repo/fsrepo"
 	intercore "github.com/ipfs/interface-go-ipfs-core"
 	"github.com/ipfs/interface-go-ipfs-core/options"
+	"go.uber.org/atomic"
+	"os"
+	"path/filepath"
+	"runtime"
 )
 
 type nodeLibIPFS struct {
@@ -59,10 +55,6 @@ func setupPlugins(externalPluginsPath string) (*loader.PluginLoader, error) {
 func newNodeLibIPFS(cfg *config.Config) *nodeLibIPFS {
 	ctx, cancel := context.WithCancel(context.Background())
 	root := filepath.Join(cfg.Path, ".ipfs")
-	//plugins, err := setupPlugins("")
-	//if err != nil {
-	//	panic(err)
-	//}
 
 	return &nodeLibIPFS{
 		ctx:        ctx,
@@ -84,119 +76,16 @@ func printVersion() {
 	fmt.Printf("Golang version: %s\n", runtime.Version())
 }
 
-// printSwarmAddrs prints the addresses of the host
-func printSwarmAddrs(node *ipfscore.IpfsNode) {
-	if !node.IsOnline {
-		fmt.Println("Swarm not listening, running in offline mode.")
-		return
-	}
-
-	var lisAddrs []string
-	ifaceAddrs, err := node.PeerHost.Network().InterfaceListenAddresses()
-	if err != nil {
-		fmt.Printf("failed to read listening addresses: %s", err)
-	}
-	for _, addr := range ifaceAddrs {
-		lisAddrs = append(lisAddrs, addr.String())
-	}
-	sort.Strings(lisAddrs)
-	for _, addr := range lisAddrs {
-		fmt.Printf("Swarm listening on %s\n", addr)
-	}
-
-	var addrs []string
-	for _, addr := range node.PeerHost.Addrs() {
-		addrs = append(addrs, addr.String())
-	}
-	sort.Strings(addrs)
-	for _, addr := range addrs {
-		fmt.Printf("Swarm announcing %s\n", addr)
-	}
-
-}
-
 // Start ...
-func (n *nodeLibIPFS) Start() (_err error) {
-	ipfsNode := startIPFSNode()
+func (n *nodeLibIPFS) Start() (err error) {
+	ipfsNode, err := startIPFSNode(n.ctx, n.configRoot)
+	if err != nil {
+		return err
+	}
 	n.CoreAPI = ipfsNode
-	//return nil
-
-	//err := mprome.Inject()
-	//if err != nil {
-	//	log.Errorf("Injecting prometheus handler for metrics failed with message: %s\n", err.Error())
-	//}
-	//// let the user know we're going.
-	//fmt.Printf("Initializing daemon...\n")
-	//
-	//defer func() {
-	//	if _err != nil {
-	//		// Print an extra line before any errors. This could go
-	//		// in the commands lib but doesn't really make sense for
-	//		// all commands.
-	//		fmt.Println(_err)
-	//	}
-	//}()
-	//
-	//printVersion()
-	//
-	//if true {
-	//	if _, _, err := utilmain.ManageFdLimit(); err != nil {
-	//		log.Errorf("setting file descriptor limit: %s", err)
-	//	}
-	//}
-	//repoPath, err := ioutil.TempDir("", "ipfs-shell")
-	//if err != nil {
-	//	return fmt.Errorf("failed to get temp dir: %s", err)
-	//}
-	//fmt.Println("repo path:", repoPath)
-	//if !fsrepo.IsInitialized(repoPath) {
-	//	if err := createRepo(n.ctx, repoPath); err != nil {
-	//		return err
-	//	}
-	//}
-	//
-	//repo, err := basis.OpenRepo(repoPath)
-	//if err != nil {
-	//	return err
-	//}
-	//n.repo = repo
-	//
-	//node, err := basis.CreateNode(n.ctx, repo)
-	//if err != nil {
-	//	return err
-	//}
-	//n.node = node
-	//node.IsDaemon = true
-	//if node.PNetFingerprint != nil {
-	//	fmt.Println("Swarm is limited to private network of peers with the swarm key")
-	//	fmt.Printf("Swarm key fingerprint: %x\n", node.PNetFingerprint)
-	//}
-	//
-	//printSwarmAddrs(node)
-	//// Attach the Core API to the constructed node
-	//
-	//_err = n.plugins.Start(node)
-	//if _err != nil {
-	//	return _err
-	//}
-	//node.Process.AddChild(goprocess.WithTeardown(n.plugins.Close))
-	//api, _err := coreapi.NewCoreAPI(node)
-	//if _err != nil {
-	//	return _err
-	//}
-	//n.CoreAPI = api
-	//
-	//errc := make(chan error)
-	//go func() {
-	//	errc <- corerepo.PeriodicGC(context.TODO(), node)
-	//	close(errc)
-	//}()
-
 	n.isRunning.Store(true)
-	log.Infow("datastore is ready")
-
-	//merge(errc)
-
+	printVersion()
+	fmt.Println("datastore is running")
 	return nil
 }
 
@@ -222,12 +111,10 @@ func (n *nodeLibIPFS) Stop() error {
 
 // Initialize ...
 func (n *nodeLibIPFS) Initialize() error {
-	log.Infow("datastore init")
-	_ = os.Mkdir(n.configRoot, 0755)
-	//if err := basis.SetupPlugins(""); err != nil {
-	//	return err
-	//}
-	// Create a Temporary Repo
+	if _, err := setupPlugins(""); err != nil {
+		return err
+	}
+	// Create a Repo
 	if err := createRepo(n.ctx, n.configRoot); err != nil {
 		return fmt.Errorf("failed to create temp repo: %s", err)
 	}
@@ -249,7 +136,6 @@ func (n *nodeLibIPFS) MessageHandle(f func(s string)) {
 }
 
 func createRepo(ctx context.Context, repoPath string) error {
-
 	identity, err := ipfsconfig.CreateIdentity(os.Stdout, []options.KeyGenerateOption{options.Key.Type(options.Ed25519Key)})
 	if err != nil {
 		return err
@@ -282,30 +168,14 @@ func badgerSpec() map[string]interface{} {
 		},
 	}
 }
-func merge(cs ...<-chan error) <-chan error {
-	var wg sync.WaitGroup
-	out := make(chan error)
 
-	// Start an output goroutine for each input channel in cs.  output
-	// copies values from c to out until c is closed, then calls wg.Done.
-	output := func(c <-chan error) {
-		for n := range c {
-			out <- n
-		}
-		wg.Done()
-	}
-	for _, c := range cs {
-		if c != nil {
-			wg.Add(1)
-			go output(c)
-		}
-	}
+func startIPFSNode(ctx context.Context, path string) (intercore.CoreAPI, error) {
+	/// --- Part I: Getting a IPFS node running
+	fmt.Println("-- Getting an IPFS node running -- ")
 
-	// Start a goroutine to close out once all the output goroutines are
-	// done.  This must start after the wg.Add call.
-	go func() {
-		wg.Wait()
-		close(out)
-	}()
-	return out
+	if _, err := setupPlugins(""); err != nil {
+		return nil, err
+	}
+	// Spawning an ephemeral IPFS node
+	return createNode(ctx, path)
 }

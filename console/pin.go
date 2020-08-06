9c1d662d87f60ed507326d7f31158012a8abb8c8
godcong <jumbycc@163.com>
2020-08-06 15:00:48 +0800

    update accipfs

--- console/pin.go
+++ console/pin.go
@@ -17,7 +17,7 @@ func pinCmd() *cobra.Command {
 		Short: "show some pin info",
 		Long:  "show the video information of pins with local server",
 	}
-	cmd.AddCommand(pinLsCmd())
+	cmd.AddCommand(pinLsCmd(), pinAddCmd())
 	return cmd
 }
 
@@ -45,6 +45,44 @@ func pinLsCmd() *cobra.Command {
 				for _, v := range pins.Pins {
 					fmt.Println(v)
 				}
+			}(ctx)
+			sigs := make(chan os.Signal)
+			signal.Notify(sigs, os.Interrupt)
+			select {
+			case <-sigs:
+				cancelFunc()
+			case v := <-done:
+				if v != nil {
+					panic(v)
+				}
+			}
+		},
+	}
+}
+
+func pinAddCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "add",
+		Short: "add pins",
+		Long:  "add hash to pin",
+		Run: func(cmd *cobra.Command, args []string) {
+			config.Initialize()
+			cfg := config.Global()
+			client.InitGlobalClient(&cfg)
+			ctx, cancelFunc := context.WithCancel(context.TODO())
+			done := make(chan error)
+			go func(c context.Context) {
+				var err error
+				defer func() {
+					done <- err
+				}()
+				resp, err := client.DataStorePinAdd(c, &core.DataStorePinAddReq{
+					Pins: args,
+				})
+				if err != nil {
+					panic(err)
+				}
+				_ = resp
 			}(ctx)
 			sigs := make(chan os.Signal)
 			signal.Notify(sigs, os.Interrupt)
